3bc1a4dcdd4cbba151adaad310f3e38c499fceb1
Phway <ilovejazz555@gmail.com>
2018-07-17 15:08:48 +0630

    firebase config file added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBkYsWDex1oQ20Ml9jN4hcjjr3TGswmwLI",
+    authDomain: "ucsy-it-camp-1531499420236.firebaseapp.com",
+    databaseURL: "https://ucsy-it-camp-1531499420236.firebaseio.com",
+    projectId: "ucsy-it-camp-1531499420236",
+    storageBucket: "ucsy-it-camp-1531499420236.appspot.com",
+    messagingSenderId: "917988140807"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBkYsWDex1oQ20Ml9jN4hcjjr3TGswmwLI",
+    authDomain: "ucsy-it-camp-1531499420236.firebaseapp.com",
+    databaseURL: "https://ucsy-it-camp-1531499420236.firebaseio.com",
+    projectId: "ucsy-it-camp-1531499420236",
+    storageBucket: "ucsy-it-camp-1531499420236.appspot.com",
+    messagingSenderId: "917988140807"
+  }
 };
 
 /*
